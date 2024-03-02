09232b2be24686fe9a3278817e6a0bc8286ae5dd
neandercode <josephjones555@yahoo.com>
2024-03-02 12:20:47 -0500

    just found out you can print multiple lines so long as you can type out sistumdotoutdotprintln

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("I'm a dumbfuck learning Java now");
+        System.out.println("I can get way over my head sometimes, but here we go");
     }
 }
