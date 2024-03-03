e34b6381d02422723095378defc2dfeef155f7f8
neandercode <josephjones555@yahoo.com>
2024-03-03 06:14:56 -0500

    comment and next

---
+++ Comment.java
@@ -0,0 +1,5 @@
+public class Comment {
+    public static void main(String args[]) {
+        System.out.println("You can't see it, but there is a comment on the right"); //this is a comment, cuh
+    }
+}
