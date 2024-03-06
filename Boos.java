0aac8454e90fe6a0ab56cb6336e848ebda41cf9c
neandercode <josephjones555@yahoo.com>
2024-03-06 04:39:06 -0500

    booleans

---
+++ Boos.java
@@ -0,0 +1,8 @@
+public class Boos {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isWhiteSauceTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isWhiteSauceTasty);
+    }
+}
