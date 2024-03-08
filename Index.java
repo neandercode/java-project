3a0247817d585cf0a5fb2a827abc6c8f57d9577e
neandercode <josephjones555@yahoo.com>
2024-03-08 05:38:46 -0500

    index of

---
+++ Index.java
@@ -0,0 +1,6 @@
+public class Index {
+    public static void main(String[] args) {
+        String txt = "I can't believe it's not butter";
+        System.out.println(txt.indexOf("believe"));
+    }
+}
