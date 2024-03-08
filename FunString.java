92f905a81bbc3c4643708cbf5889f0a893a26a7b
neandercode <josephjones555@yahoo.com>
2024-03-08 05:19:20 -0500

    do it

---
+++ FunString.java
@@ -0,0 +1,7 @@
+public class FunString {
+    public static void main(String[] args) {
+    String txt = "Go watch anime";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+}
+}
