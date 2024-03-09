846e156f4b36a367b75113b40316200a8a835ba7
neandercode <josephjones555@yahoo.com>
2024-03-09 06:25:36 -0500

    concatenation

---
+++ Concat.java
@@ -0,0 +1,7 @@
+public class Concat {
+    public static void main(String[] args) {
+        String firstName = "Monkey D.";
+        String lastName = "Luffy";
+        System.out.println(firstName + " " + lastName);
+    }
+}
