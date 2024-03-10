1b36f3d1ed00f5e5c57a34a1adb80da4ae26e255
neandercode <josephjones555@yahoo.com>
2024-03-10 07:11:10 -0400

    if else

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int time = 22;
+        if (time < 10) {
+            System.out.println("buenos dias, chingon!");
+        } else if (time < 18) {
+            System.out.println("I say, good day to you!");
+        } else {
+            System.out.println("It's quittin time!");
+        }
+    }
+}
